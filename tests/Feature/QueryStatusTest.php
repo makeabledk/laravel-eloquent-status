0d8600a7df7a05e95d2373d22a283ecca229e767
Rasmus Nielsen <rasmus@easify.dk>
2019-09-13 11:01:35 +0200

    Bump dependencies

--- tests/Feature/QueryStatusTest.php
+++ tests/Feature/QueryStatusTest.php
@@ -10,7 +10,7 @@ use Makeable\EloquentStatus\Tests\TestCase;
 
 class QueryStatusTest extends TestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
